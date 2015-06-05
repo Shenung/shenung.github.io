0a0898fd5833f101c7a6e490712f5ca0e2c3b81e
shenung <shenung@mail.fresnostate.edu>
2015-06-05 01:47:57 -0700

    using new IDE

--- js/test.js
+++ js/test.js
@@ -85,7 +85,17 @@
 //}
 
 //-----------------8 BALL------------------------------------------------
-
+//
+//var images = 
+//['http://planetoftheweb.com/i/8ball/19.png', 'http://planetoftheweb.com/i/8ball/18.png', 'http://planetoftheweb.com/i/8ball/17.png', 'http://planetoftheweb.com/i/8ball/16.png', 'http://planetoftheweb.com/i/8ball/15.png', 'http://planetoftheweb.com/i/8ball/14.png', 'http://planetoftheweb.com/i/8ball/13.png', 'http://planetoftheweb.com/i/8ball/12.png', 'http://planetoftheweb.com/i/8ball/11.png', 'http://planetoftheweb.com/i/8ball/10.png', 'http://planetoftheweb.com/i/8ball/9.png', 'http://planetoftheweb.com/i/8ball/8.png', 'http://planetoftheweb.com/i/8ball/7.png', 'http://planetoftheweb.com/i/8ball/6.png', 'http://planetoftheweb.com/i/8ball/5.png', 'http://planetoftheweb.com/i/8ball/4.png', 'http://planetoftheweb.com/i/8ball/3.png', 'http://planetoftheweb.com/i/8ball/2.png', 'http://planetoftheweb.com/i/8ball/1.png', 'http://planetoftheweb.com/i/8ball/0.png'];
+//
+//
+//
+//document.querySelector('.btn').addEventListener('click', function () {
+//  
+//  var imageNum = Math.floor (Math.random (images.length)*19);
+//  document.querySelector('body').style.backgroundImage = 'url('+images[imageNum]+')';
+//}, false);
 
 //-----------------------------------------------------------------------
 //function printDay(){
