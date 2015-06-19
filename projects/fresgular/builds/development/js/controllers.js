8da8a8d16125da38aa06f956f6cc9c9442f9249f
shenung <shenung@mail.fresnostate.edu>
2015-06-19 11:58:19 -0700

    Added Image uploading

---
+++ projects/fresgular/builds/development/js/controllers.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var artistControllers = angular.module('artistControllers', ['ngAnimate']);
+
+artistControllers.controller('ListController',
+	['$scope', 'GetData', 'FIREBASE_URL',
+	function($scope, GetData, FIREBASE_URL) {
+		$scope.artists = GetData;
+    $scope.artistOrder = 'name';
+		var ref = new Firebase(FIREBASE_URL);
+		$scope.showsearch = true;
+		$scope.showadd = false;
+
+		$scope.onAdd = function() {
+          //Added image uploading
+            var imageReader = new FileReader();
+            imageReader.addEventListener('load', function(e){
+              $scope.user.image = e.target.result;
+              ref.push($scope.user);
+            });
+            imageReader.readAsDataURL(document.forms.adder.imageupload.files[0]);
+		};
+
+		$scope.deleteItem = function(deleteID) {
+			console.log(deleteID);
+			var deleteRef = new Firebase(FIREBASE_URL + deleteID);
+			deleteRef.remove();
+		};
+}]);
+
+artistControllers.controller('DetailsController',
+	['$scope', 'GetData','$routeParams',
+	function($scope, GetData, $routeParams) {
+		$scope.artists = GetData;
+    $scope.whichItem = $routeParams.itemId;
+}]);
